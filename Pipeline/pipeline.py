cee01b02042f93a3c87d8cb72adfb46ecd475775
Aman Chandra <amanchandra1650@gmail.com>
2026-01-03 15:25:58 +0530

    Pipeline

--- Pipeline/pipeline.py
+++ Pipeline/pipeline.py
@@ -9,4 +9,6 @@ df = pd.DataFrame({"Day": [1,2], "num_passenger": [3,4]})
 df['month'] = month
 print(df.head())
 
+df.to_parquet(f"output_{month}.parquet")
+
 print(f"Hello pipline, month = {month}")
